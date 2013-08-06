0c4ee39849f845480a083362ea40523419ad94a4
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-06 23:15:14 +0200

    basic emacs mode

---
+++ dotlang-mode.el
@@ -0,0 +1,13 @@
+(require 'generic-x)
+(define-generic-mode
+    'dotlang-mode
+  '("#")
+  '("while" "if" "def" "arg")
+  '( ; builtin
+    ("[a-zA-Z0-9_@!?-]+\\." . 'font-lock-builtin-face)
+    ("\\.[a-zA-Z0-9_@!?-]+" . 'font-lock-function-name-face))
+  '("\\.dot$")
+  nil
+  "A mode for dotlang files")
+
+(provide 'dotlang-mode)
