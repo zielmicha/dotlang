27e35b9553d34ec1ee47188e892cc0b6478b580c
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-06 23:51:38 +0200

    dotlang-mode.el: better coloring

--- dotlang-mode.el
+++ dotlang-mode.el
@@ -4,8 +4,9 @@
   '("#")
   '("while" "if" "def" "arg")
   '( ; builtin
-    ("[a-zA-Z0-9_@!?-]+\\." . 'font-lock-builtin-face)
-    ("\\.[a-zA-Z0-9_@!?-]+" . 'font-lock-function-name-face))
+    ("[a-zA-Z0-9_@!?-]+\\." . 'font-lock-variable-face)
+    ("\\.[a-zA-Z0-9_@!?-]+" . 'font-lock-builtin-face)
+    ("[a-zA-Z_@!?-][a-zA-Z0-9_@!?-]*" . 'font-lock-function-name-face))
   '("\\.dot$")
   nil
   "A mode for dotlang files")
