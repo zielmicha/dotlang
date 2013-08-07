b2d222e2f34fe7c110d244ffbb6343c6e895e034
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-08 00:38:14 +0200

    interactive interpreter

--- dot/__main__.py
+++ dot/__main__.py
@@ -1,9 +1,34 @@
-from sys import stdin, argv
+from sys import argv
 
 from dot.parse import parse
 from dot.runtime import * # __main__ --> dot
 
-if len(argv) == 2: stdin = open(argv[1])
+def run_string(s):
+    code = parse(s)
+    return RootFrame(Environ(parents=[builtins]), code).run()
 
-code = parse(stdin.read())
-RootFrame(Environ(parents=[builtins]), code).run()
+if len(argv) == 1:
+    import traceback
+    import os
+    import readline
+    histfile = os.path.join(os.path.expanduser("~"), ".dot_history")
+    try:
+        readline.read_history_file(histfile)
+    except IOError:
+        pass
+    import atexit
+    atexit.register(readline.write_history_file, histfile)
+    del os, histfile
+
+    while True:
+        try:
+            s = raw_input('> ')
+        except (KeyboardInterrupt, EOFError):
+            break
+        try:
+            print repr(run_string(s))
+        except:
+            traceback.print_exc()
+else:
+    stdin = open(argv[1])
+    run_string(stdin.read())
