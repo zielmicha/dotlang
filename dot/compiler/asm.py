ce6f587d00405ca33a9217ddeb951fe3c792bc96
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-08 23:39:49 +0200

    compiler compiles 'Hello world' example

--- dot/compiler/asm.py
+++ dot/compiler/asm.py
@@ -31,16 +31,50 @@ def assemble_const(s):
     return [(bp.LOAD_CONST, s)]
 
 def assemble_call(name, arg_count):
+    call_success = bp.Label()
+    end = bp.Label()
     return [(bp.BUILD_TUPLE, arg_count),
+            # stack = [args]
+            (bp.DUP_TOP, None),
+            # stack = [args, args]
             (bp.LOAD_GLOBAL, '_dotlang_call'),
-#            (bp.ROT_TWO, None),
+            (bp.ROT_TWO, None),
+            # stack = [args, _dotlang_call, args]
             (bp.LOAD_CONST, name),
-            (bp.CALL_FUNCTION, 2)]
+            # stack = [args, _dotlang_call, args, name]
+            (bp.CALL_FUNCTION, 2),
+            # stack = [args, result]
+            (bp.DUP_TOP, None),
+            # stack = [args, result, result]
+            (bp.LOAD_CONST, NotImplemented),
+            # stack = [args, result, result, NotImplemented]
+            (bp.COMPARE_OP, 'is'),
+            # stack = [args, result, success?]
+            (bp.POP_JUMP_IF_FALSE, call_success),
+            # stack = [args, result]
+            (bp.POP_TOP, None),
+            # stack = [args]
+            (bp.LOAD_GLOBAL, 'func-' + name),
+            # stack = [args, func]
+            (bp.ROT_TWO, None),
+            # stack = [func, args]
+            (bp.CALL_FUNCTION_VAR, 0),
+            # stack = [result]
+            (bp.JUMP_FORWARD, end),
+            (call_success, None),
+            # stack = [args, result]
+            (bp.ROT_TWO, None),
+            (bp.POP_TOP, None),
+            # stack = [result]
+            (end, None),
+        ]
 
 if __name__ == '__main__':
     from sys import argv, stdin
-    from dot.parse import parse
-    from dot.compiler.builder import Builder
+    import dot.parse
+    import dot.compiler.builder
+    import dot.runtime
+
     import pprint
 
     filename = '<stdin>'
@@ -48,10 +82,10 @@ if __name__ == '__main__':
         stdin = open(argv[1])
         filename = argv[1]
 
-    ast = parse(stdin.read())
-    b = Builder(filename)
+    ast = dot.parse.parse(stdin.read())
+    b = dot.compiler.builder.Builder(filename)
     b.visit(ast)
     bp_code = assemble(b)
-    print bp_code.code
+    #print bp_code.code
     py_code = bp_code.to_code()
-    exec py_code
+    dot.runtime.execute(py_code)
--- dot/runtime.py
+++ dot/runtime.py
@@ -1,1 +1,10 @@
--
+from dot.lib.core import builtins
+
+def call(a, b):
+    return NotImplemented
+
+def execute(executable):
+    globals = {'__builtins__': builtins}
+    return eval(executable, globals)
+
+builtins['_dotlang_call'] = call
